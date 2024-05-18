a71ec87118a1a1f33974622913ca6ca83964e5b1
brendan <brendangooch@gmail.com>
2024-05-18 22:09:00 +0100

    finished index.ts for now (font loading functionality to add)

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,34 @@
  * 
  */
 
-// import Canvas from "./module/canvas.js";
+import Canvas from "./module/canvas.js";
+import runTests from './tests.js';
 
-(() => {
+(async () => {
 
     window.onload = () => {
 
-        // const canvas = new Canvas();
+        Canvas.HTML_ID = 'canvas';
+        Canvas.WIDTH = window.innerWidth;
+        Canvas.HEIGHT = window.innerHeight;
 
+        try {
+
+            const canvas = new Canvas();
+
+            // load images
+            const images: HTMLImageElement[] = [];
+
+            // load fonts
+
+            // run tests
+            runTests(canvas, images);
+
+
+        } catch (error) {
+            console.error(error);
+
+        }
 
     };
 
