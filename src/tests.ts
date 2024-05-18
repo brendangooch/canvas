a0d1d3222006e684d5f2d2f8eddc083050690f0a
brendan <brendangooch@gmail.com>
2024-05-18 22:10:04 +0100

    finished skeleton of tests.ts (tests to be added at later stage)

--- src/tests.ts
+++ src/tests.ts
@@ -2,7 +2,11 @@
  * tests for the Canvas module
  */
 
+import Canvas from "./module/canvas.js";
+
 // all tests - comment/uncomment as required
-export default function (): void {
-    //
+export default function (canvas: Canvas, images: HTMLImageElement[]): void {
+    console.log(canvas); // tmp
+    console.log(images); // tmp
+
 }
