a55fc3754bbf3b64bb356c56ff12df4c09edd908
brendan <brendangooch@gmail.com>
2024-05-18 21:27:41 +0100

    added initial functionality to Canvas class

--- src/module/canvas.ts
+++ src/module/canvas.ts
@@ -2,8 +2,34 @@
  * 
  */
 
-export default class {
+export default class Canvas {
 
-    //
+    public static HTML_ID: string = 'canvas';
+    public static WIDTH: number = window.innerWidth;
+    public static HEIGHT: number = window.innerHeight;
+
+    private canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById(Canvas.HTML_ID);
+    public ctx: CanvasRenderingContext2D = this.canvas.getContext('2d')!;
+
+    public constructor() {
+        this.canvas.width = Canvas.WIDTH;
+        this.canvas.width = Canvas.HEIGHT;
+    }
+
+    public get width(): number {
+        return this.canvas.width;
+    }
+
+    public get height(): number {
+        return this.canvas.height;
+    }
+
+    public get center(): { x: number; y: number } {
+        return {
+            x: this.width / 2,
+            y: this.height / 2
+        };
+    }
+
 
 }
