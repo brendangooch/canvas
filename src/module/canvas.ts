51d38b68a52cad3eff2fb00f6e07b80f4ee21c66
brendan <brendangooch@gmail.com>
2024-05-18 21:54:45 +0100

    Canvas class now throws an error if no canvas element with id=canvas present in html file

--- src/module/canvas.ts
+++ src/module/canvas.ts
@@ -8,10 +8,13 @@ export default class Canvas {
     public static WIDTH: number = window.innerWidth;
     public static HEIGHT: number = window.innerHeight;
 
-    private canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById(Canvas.HTML_ID);
-    public ctx: CanvasRenderingContext2D = this.canvas.getContext('2d')!;
+    private canvas: HTMLCanvasElement;
+    public ctx: CanvasRenderingContext2D;
 
     public constructor() {
+        this.verifyElementExists();
+        this.canvas = <HTMLCanvasElement>document.getElementById(Canvas.HTML_ID);
+        this.ctx = this.canvas.getContext('2d')!;
         this.canvas.width = Canvas.WIDTH;
         this.canvas.width = Canvas.HEIGHT;
     }
@@ -31,5 +34,11 @@ export default class Canvas {
         };
     }
 
+    // test that a canvas with id = canvas exists in index.html
+    private verifyElementExists(): void {
+        if (document.getElementById(Canvas.HTML_ID) === null) {
+            throw new Error('the page MUST contain a canvas element with id = canvas to use the Canvas class');
+        }
+    }
 
 }
