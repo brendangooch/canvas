ba0dd4bed7dead0497f4029929468c8d9db3672e
brendan <brendangooch@gmail.com>
2024-05-19 16:28:16 +0100

    added unimplemented functionality to canvas path test file

--- src/tests/test-path.ts
+++ src/tests/test-path.ts
@@ -7,5 +7,152 @@ import Canvas from "../module/canvas.js";
 // all tests - comment/uncomment as required
 // @ts-ignore
 export default function (canvas: Canvas): void {
-    //
+    // testBegin(canvas);
+    // testFill(canvas);
+    // testFill2(canvas);
+    // testStroke(canvas);
+    // testClose(canvas);
+    // testClip(canvas);
+    // testClip2(canvas);
+    // testMoveTo(canvas);
+    // testLineTo(canvas);
+    // testArcTo(canvas);
+    // testBezierCurveTo(canvas);
+    // testQuadraticCurveTo(canvas);
+    // testLine(canvas);
+    // testArc(canvas);
+    // testRect(canvas);
+    // testEllipse(canvas);
+    // testCircle(canvas);
+    // testSquare(canvas);
+    // testIsPointInPath(canvas);
+    // testIsPointInPath2(canvas);
+    // testIsPointInStroke(canvas);
+
+}
+
+// public begin(): void
+// @ts-ignore
+function testBegin(canvas: Canvas): void {
+
+}
+
+// public fill(): void
+// @ts-ignore
+function testFill(canvas: Canvas): void {
+
+}
+
+// public fill2(): void
+// @ts-ignore
+function testFill2(canvas: Canvas): void {
+
+}
+
+// public stroke(): void
+// @ts-ignore
+function testStroke(canvas: Canvas): void {
+
+}
+
+// public close(): void
+// @ts-ignore
+function testClose(canvas: Canvas): void {
+
+}
+
+// public clip(): void
+// @ts-ignore
+function testClip(canvas: Canvas): void {
+
+}
+
+// public clip2(): void
+// @ts-ignore
+function testClip2(canvas: Canvas): void {
+
+}
+
+// public moveTo(x: number, y: number): void
+// @ts-ignore
+function testMoveTo(canvas: Canvas): void {
+
+}
+
+// public lineTo(x: number, y: number): void
+// @ts-ignore
+function testLineTo(canvas: Canvas): void {
+
+}
+
+// public arcTo(x1: number, y1: number, x2: number, y2: number, radius: number): void
+// @ts-ignore
+function testArcTo(canvas: Canvas): void {
+
+}
+
+// public bezierCurveTo(cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number): void
+// @ts-ignore
+function testBezierCurveTo(canvas: Canvas): void {
+
+}
+
+// public quadraticCurveTo(cpx: number, cpy: number, x: number, y: number): void
+// @ts-ignore
+function testQuadraticCurveTo(canvas: Canvas): void {
+
+}
+
+// public line(x1: number, y1: number, x2: number, y2: number): void
+// @ts-ignore
+function testLine(canvas: Canvas): void {
+
+}
+
+// public arc(radius: number, startAngle: number, endAngle: number, counterclockwise: boolean = false): void
+// @ts-ignore
+function testArc(canvas: Canvas): void {
+
+}
+
+// public rect(width: number, height: number, radii: number | number[] = 0): void
+// @ts-ignore
+function testRect(canvas: Canvas): void {
+
+}
+
+// public ellipse(x: number, y: number, radiusX: number, radiusY: number, rotation: number, startAngle: number, endAngle: number, counterclockwise: boolean = false): void
+// @ts-ignore
+function testEllipse(canvas: Canvas): void {
+
+}
+
+// public circle(radius: number): void
+// @ts-ignore
+function testCircle(canvas: Canvas): void {
+
+}
+
+// public square(length: number, radii: number | number[] = 0): void
+// @ts-ignore
+function testSquare(canvas: Canvas): void {
+
+}
+
+// public isPointInPath(x: number, y: number): boolean
+// @ts-ignore
+function testIsPointInPath(canvas: Canvas): void {
+
+}
+
+// public isPointInPath2(x: number, y: number): boolean
+// @ts-ignore
+function testIsPointInPath2(canvas: Canvas): void {
+
+}
+
+// public isPointInStroke(x: number, y: number): boolean
+// @ts-ignore
+function testIsPointInStroke(canvas: Canvas): void {
+
 }
