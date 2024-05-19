f18bda67a384cd1f348bc5547154a30aa2c95a01
brendan <brendangooch@gmail.com>
2024-05-19 20:39:36 +0100

    created a custom-types.d.ts file for custom types use by the module; added tTranslationOption

---
+++ src/module/custom-types.d.ts
@@ -0,0 +1,5 @@
+/**
+ * custom types shared by classes in the canvas module
+ */
+
+export type tTranslateOption = 'center' | 'top' | 'bottom' | 'left' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
