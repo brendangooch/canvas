f4d0dae8cf3e182b4951c6c52c16f75962d057f3
brendan <brendangooch@gmail.com>
2024-05-19 15:39:36 +0100

    finished implementation for canvas path module (to be tested)

--- src/module/canvas-path.ts
+++ src/module/canvas-path.ts
@@ -10,8 +10,14 @@ export default class CanvasPath extends CanvasComponent {
         this.ctx.beginPath();
     }
 
+    // non-zero
     public fill(): void {
         this.ctx.fill();
+    }
+
+    // even-odd
+    public fill2(): void {
+        this.ctx.fill('evenodd');
     }
 
     // stroke()
@@ -20,22 +26,94 @@ export default class CanvasPath extends CanvasComponent {
     }
 
     // closePath()
-    // clip()
+    public close(): void {
+        this.ctx.closePath();
+    }
+
+    // non-zero
+    public clip(): void {
+        this.ctx.clip();
+    }
+
+    // even-odd
+    public clip2(): void {
+        this.ctx.clip('evenodd');
+    }
+
+    // moveTo()
+    public moveTo(x: number, y: number): void {
+        this.ctx.moveTo(x, y);
+    }
 
     // lineTo()
-    // moveTo()
+    public lineTo(x: number, y: number): void {
+        this.ctx.lineTo(x, y);
+    }
+
     // arcTo()
+    public arcTo(x1: number, y1: number, x2: number, y2: number, radius: number): void {
+        this.ctx.arcTo(x1, y1, x2, y2, radius);
+    }
+
     // bezierCurveTo()
+    public bezierCurveTo(cp1x: number, cp1y: number, cp2x: number, cp2y: number, x: number, y: number): void {
+        this.ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+    }
+
     // quadraticCurveTo()
+    public quadraticCurveTo(cpx: number, cpy: number, x: number, y: number): void {
+        this.ctx.quadraticCurveTo(cpx, cpy, x, y);
+    }
+
+    // line convenience method
+    public line(x1: number, y1: number, x2: number, y2: number): void {
+        this.moveTo(x1, y1);
+        this.lineTo(x2, y2);
+    }
 
     // arc()
+    public arc(radius: number, startAngle: number, endAngle: number, counterclockwise: boolean = false): void {
+        this.ctx.arc(0, 0, radius, startAngle, endAngle, counterclockwise);
+    }
+
     // rect() / roundRect()
+    public rect(width: number, height: number, radii: number | number[] = 0): void {
+        // roundRect available
+        if (typeof this.ctx.roundRect === 'function') {
+            this.ctx.roundRect(-width / 2, -height / 2, width, height, radii);
+        }
+        else {
+            this.ctx.rect(-width / 2, -height / 2, width, height);
+        }
+    }
+
     // ellipse()
-    // circle()
-    // square()
+    public ellipse(x: number, y: number, radiusX: number, radiusY: number, rotation: number, startAngle: number, endAngle: number, counterclockwise: boolean = false): void {
+        this.ctx.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, counterclockwise);
+    }
 
-    // isPointInPath()
-    // isPointInStroke()
+    // create a complete circle
+    public circle(radius: number): void {
+        this.arc(radius, 0, Math.PI * 2);
+    }
 
+    // create a square
+    public square(length: number, radii: number | number[] = 0): void {
+        this.rect(length, length, radii);
+    }
+
+    // non-zero
+    public isPointInPath(x: number, y: number): boolean {
+        return this.ctx.isPointInPath(x, y);
+    }
+
+    // even-odd
+    public isPointInPath2(x: number, y: number): boolean {
+        return this.ctx.isPointInPath(x, y, 'evenodd');
+    }
+
+    public isPointInStroke(x: number, y: number): boolean {
+        return this.ctx.isPointInStroke(x, y);
+    }
 
 }
