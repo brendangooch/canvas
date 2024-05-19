115bee22e415cad6ba8bf224c8aaa92b54a429b6
brendan <brendangooch@gmail.com>
2024-05-19 10:50:26 +0100

    canvas class skeleton complete; implementation to follow

--- src/module/canvas.ts
+++ src/module/canvas.ts
@@ -2,6 +2,17 @@
  * 
  */
 
+import CanvasColor from "./canvas-color.js";
+import CanvasCompositeOperation from "./canvas-composite-operation.js";
+import CanvasFilter from "./canvas-filter.js";
+import CanvasImageData from "./canvas-image-data.js";
+import CanvasImage from "./canvas-image.js";
+import CanvasLine from "./canvas-line.js";
+import CanvasPath from "./canvas-path.js";
+import CanvasShadow from "./canvas-shadow.js";
+import CanvasText from "./canvas-text.js";
+import CanvasTransform from "./canvas-transform.js";
+
 export default class Canvas {
 
     public static HTML_ID: string = 'canvas';
@@ -10,24 +21,32 @@ export default class Canvas {
 
     public ctx: CanvasRenderingContext2D;
     private canvas: HTMLCanvasElement;
-
-    // path(): CanvasPath
-    // text(): CanvasText
-    // color(): CanvasColor
-    // image(): CanvaImage
-    // filter(): CanvasFilter
-    // shadow(): CanvasShadow
-    // line(): CanvasLine
-    // imageData(): CanvasImageData
-    // composite(): CanvasCompositeOperation
-    // transform(): CanvasTransform
+    private _path: CanvasPath;
+    private _text: CanvasText;
+    private _color: CanvasColor;
+    private _image: CanvasImage;
+    private _filter: CanvasFilter;
+    private _shadow: CanvasShadow;
+    private _line: CanvasLine;
+    private _imageData: CanvasImageData;
+    private _composite: CanvasCompositeOperation;
+    private _transform: CanvasTransform;
 
     public constructor() {
-        this.verifyElementExists();
+        this.checkElementExists();
         this.canvas = <HTMLCanvasElement>document.getElementById(Canvas.HTML_ID);
         this.ctx = this.canvas.getContext('2d')!;
+        this._path = new CanvasPath(this.ctx);
+        this._text = new CanvasText(this.ctx);
+        this._color = new CanvasColor(this.ctx);
+        this._image = new CanvasImage(this.ctx);
+        this._filter = new CanvasFilter(this.ctx);
+        this._shadow = new CanvasShadow(this.ctx);
+        this._line = new CanvasLine(this.ctx);
+        this._imageData = new CanvasImageData(this.ctx);
+        this._composite = new CanvasCompositeOperation(this.ctx);
+        this._transform = new CanvasTransform(this.ctx);
         this.setDimensions();
-        this.loadComponents();
     }
 
     public get width(): number {
@@ -45,16 +64,45 @@ export default class Canvas {
         };
     }
 
-    // path(): CanvasPath
-    // text(): CanvasText
-    // color(): CanvasColor
-    // image(): CanvaImage
-    // filter(): CanvasFilter
-    // shadow(): CanvasShadow
-    // line(): CanvasLine
-    // imageData(): CanvasImageData
-    // composite(): CanvasCompositeOperation
-    // transform(): CanvasTransform
+    public get path(): CanvasPath {
+        return this._path;
+    }
+
+    public get text(): CanvasText {
+        return this._text;
+    }
+
+    public get color(): CanvasColor {
+        return this._color;
+    }
+
+    public get image(): CanvasImage {
+        return this._image;
+    }
+
+    public get filter(): CanvasFilter {
+        return this._filter;
+    }
+
+    public get shadow(): CanvasShadow {
+        return this._shadow;
+    }
+
+    public get line(): CanvasLine {
+        return this._line;
+    }
+
+    public get imageData(): CanvasImageData {
+        return this._imageData;
+    }
+
+    public get composite(): CanvasCompositeOperation {
+        return this._composite;
+    }
+
+    public get transform(): CanvasTransform {
+        return this._transform;
+    }
 
     //getContextAttributes()
     public get attributes(): {} {
@@ -119,7 +167,7 @@ export default class Canvas {
     }
 
     // test that a canvas with id = canvas exists in index.html
-    private verifyElementExists(): void {
+    private checkElementExists(): void {
         if (document.getElementById(Canvas.HTML_ID) === null) {
             throw new Error('the page MUST contain a canvas element with id = canvas to use the Canvas class');
         }
@@ -129,20 +177,6 @@ export default class Canvas {
     private setDimensions(): void {
         this.canvas.width = Canvas.WIDTH;
         this.canvas.width = Canvas.HEIGHT;
-    }
-
-    // loadComponents()
-    private loadComponents(): void {
-        // path(): CanvasPath
-        // text(): CanvasText
-        // color(): CanvasColor
-        // image(): CanvaImage
-        // filter(): CanvasFilter
-        // shadow(): CanvasShadow
-        // line(): CanvasLine
-        // imageData(): CanvasImageData
-        // composite(): CanvasCompositeOperation
-        // transform(): CanvasTransform
     }
 
 }
