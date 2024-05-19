b3e8245b8eb3630fba4aece599335903dc35ace7
brendan <brendangooch@gmail.com>
2024-05-19 12:32:21 +0100

    implemented a few basic methods on canvas paths component (not tested)

--- src/module/canvas-path.ts
+++ src/module/canvas-path.ts
@@ -6,18 +6,36 @@ import CanvasComponent from "./canvas-component.js";
 
 export default class CanvasPath extends CanvasComponent {
 
-    // beginPath()
+    public begin(): void {
+        this.ctx.beginPath();
+    }
+
+    public fill(): void {
+        this.ctx.fill();
+    }
+
+    // stroke()
+    public stroke(): void {
+        this.ctx.stroke();
+    }
+
+    // closePath()
+    // clip()
+
     // lineTo()
     // moveTo()
     // arcTo()
     // bezierCurveTo()
     // quadraticCurveTo()
+
     // arc()
     // rect() / roundRect()
-    // clip()
-    // closePath()
     // ellipse()
+    // circle()
+    // square()
+
     // isPointInPath()
     // isPointInStroke()
+
 
 }
