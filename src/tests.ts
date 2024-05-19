756d112208324b1732a25b09524512952f535146
brendan <brendangooch@gmail.com>
2024-05-19 11:07:24 +0100

    added all test functions (without implementation) to manual tests file

--- src/tests.ts
+++ src/tests.ts
@@ -5,8 +5,112 @@
 import Canvas from "./module/canvas.js";
 
 // all tests - comment/uncomment as required
+// @ts-ignore
 export default function (canvas: Canvas, images: HTMLImageElement[]): void {
-    console.log(canvas); // tmp
-    console.log(images); // tmp
+    // testGetWidth(canvas);
+    // testGetHeight(canvas);
+    // testGetCenter(canvas);
+    // testClear(canvas);
+    // testSave(canvas);
+    // testReset(canvas);
+    // testSetAlpha(canvas);
+    // testScale(canvas);
+    // testTranslate(canvas);
+    // testRotate(canvas);
+    // testFill(canvas);
+    // testStroke(canvas);
+    // testGetAttributes(canvas);
+    // testIsContextLost(canvas);
+    // testEnableSmoothing(canvas);
+    // testDisableSmoothing(canvas);
+    // testPath(canvas);
+    // testText(canvas);
+    // testColor(canvas);
+    // testImage(canvas);
+    // testFilter(canvas);
+    // testShadow(canvas);
+    // testLine(canvas);
+    // testImageData(canvas);
+    // testComposite(canvas);
+    // testTransform(canvas);
 
 }
+
+
+// @ts-ignore
+function testGetWidth(canvas: Canvas): void { }
+
+// @ts-ignore
+function testGetHeight(canvas: Canvas): void { }
+
+// @ts-ignore
+function testGetCenter(canvas: Canvas): void { }
+
+// @ts-ignore
+function testClear(canvas: Canvas): void { }
+
+// @ts-ignore
+function testSave(canvas: Canvas): void { }
+
+// @ts-ignore
+function testReset(canvas: Canvas): void { }
+
+// @ts-ignore
+function testSetAlpha(canvas: Canvas): void { }
+
+// @ts-ignore
+function testScale(canvas: Canvas): void { }
+
+// @ts-ignore
+function testTranslate(canvas: Canvas): void { }
+
+// @ts-ignore
+function testRotate(canvas: Canvas): void { }
+
+// @ts-ignore
+function testFill(canvas: Canvas): void { }
+
+// @ts-ignore
+function testStroke(canvas: Canvas): void { }
+
+// @ts-ignore
+function testGetAttributes(canvas: Canvas): void { }
+
+// @ts-ignore
+function testIsContextLost(canvas: Canvas): void { }
+
+// @ts-ignore
+function testEnableSmoothing(canvas: Canvas): void { }
+
+// @ts-ignore
+function testDisableSmoothing(canvas: Canvas): void { }
+
+// @ts-ignore
+function testPath(canvas: Canvas): void { }
+
+// @ts-ignore
+function testText(canvas: Canvas): void { }
+
+// @ts-ignore
+function testColor(canvas: Canvas): void { }
+
+// @ts-ignore
+function testImage(canvas: Canvas): void { }
+
+// @ts-ignore
+function testFilter(canvas: Canvas): void { }
+
+// @ts-ignore
+function testShadow(canvas: Canvas): void { }
+
+// @ts-ignore
+function testLine(canvas: Canvas): void { }
+
+// @ts-ignore
+function testImageData(canvas: Canvas): void { }
+
+// @ts-ignore
+function testComposite(canvas: Canvas): void { }
+
+// @ts-ignore
+function testTransform(canvas: Canvas): void { }
