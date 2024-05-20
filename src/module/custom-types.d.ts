afb55a5c86e2c79a76d663b8a6b2b45c0eaaa468
brendan <brendangooch@gmail.com>
2024-05-20 18:06:13 +0100

    removed the custom types file; safer for libary not to export types as they could clash with other types in app

--- src/module/custom-types.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * custom types shared by classes in the canvas module
- */
-
-export type tTranslateOption = 'center' | 'top' | 'bottom' | 'left' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
