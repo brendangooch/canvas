3185aba6d6c19ff5f48dae986c5a6d8166b83bb0
brendan <brendangooch@gmail.com>
2024-05-20 20:46:01 +0100

    implemented and manually tested load-font.ts file

--- src/utils/load-font.ts
+++ src/utils/load-font.ts
@@ -2,4 +2,12 @@
  * load a font
  */
 
-//
+export default function (name: string, path: string, desciptors: FontFaceDescriptors = {}): Promise<void> {
+    const url = `url(${path})`;
+    return new Promise(async (res) => {
+        const font = new FontFace(name, url, desciptors);
+        document.fonts.add(font);
+        await font.load();
+        res();
+    });
+}
